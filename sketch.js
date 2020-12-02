ce99fb9281cd65b589b1aa32aa13984b997822b7
bianchinicecilia <bianchini.cecilia@gmail.com>
2020-12-02 00:20:23 +0000

    3D model

--- sketch.js
+++ sketch.js
@@ -1,12 +1,21 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/NCCHQwNAN6Y
+
 
 var song;
 var button;
 var amp;
 var canvas;
+
+let angle;
+let extraCanvas;
+
+let kitten;
+let train;
+
+
+function preload() {
+  kitten = loadImage('kittens/kitten2.jpg');
+  train = loadModel('train-corrected.obj');
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
@@ -14,17 +23,17 @@ function windowResized() {
 
 function setup() {
   canvas = createCanvas (windowWidth, windowHeight);
-  /*createCanvas(windowWidth, windowHeight,WEBGL);*/
   canvas.position (0, 0);
   canvas.style('z-index', '-3');
   song = loadSound('yume.mp3', loaded);
   amp = new p5.Amplitude();
   background('#F4EDED');
+  extraCanvas = createGraphics(windowWidth, windowHeight, WEBGL);
+  extraCanvas.clear();
 }
 
 function loaded() {
-  button = createButton('Play');
-  /*button.mousePressed(info);*/
+  button = createButton('play');
   button.mousePressed(togglePlaying);
   button.position(10,10);
   button.style('background-color', '#F4EDED');
@@ -32,11 +41,8 @@ function loaded() {
   button.style('font-size', '1.5em');
 }
 
-/*function info() {
-  rect(100,100,100,100);
-}*/
-
 function draw() {
+  //sound reaction
   background('#F4EDED');
 
   var vol = amp.getLevel();
@@ -44,16 +50,32 @@ function draw() {
 
   fill('#FFFFFF');
   noStroke();
-  ellipse(windowWidth / 2, windowHeight / 2, diam, diam);
+  ellipse(width / 2, height / 2, diam, diam);
+
+
+  // obj
+  extraCanvas.ambientLight(50);
+  extraCanvas.directionalLight(255, 255, 255, 0, 0, 1);
+  extraCanvas.rotateZ(frameCount * 0.00005);
+  extraCanvas.rotateX(frameCount * 0.00005);
+  extraCanvas.rotateY(frameCount * 0.00005);
+  extraCanvas.clear();
+  extraCanvas.translate(0, 0, 0);
+  extraCanvas.texture(kitten);
+  extraCanvas.model(train);
+  extraCanvas.noStroke();
+
+  image(extraCanvas, 0, 0);
+
 }
 
 function togglePlaying() {
   if (!song.isPlaying()) {
     song.play();
     song.setVolume(0.3);
-    button.html('Pause');
+    button.html('pause');
   } else {
     song.stop();
-    button.html('Play');
+    button.html('play');
   }
 }
