8072417010a374a27fb34e763c09529680782ccf
bianchinicecilia <bianchini.cecilia@gmail.com>
2020-12-13 15:51:59 +0000

    fixes mobile

--- sketch.js
+++ sketch.js
@@ -107,9 +107,17 @@ function draw() {
 
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "black";
+  } else {
+   document.body.style.backgroundColor = "pink";
+  }
 }
+
+var x = window.matchMedia("(max-width: 550px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 
 function togglePlaying() {
   if (!song.isPlaying()) {
