c1d72d2693d81a54dc86afc821bf87bba54a5bd2
bianchinicecilia <bianchini.cecilia@gmail.com>
2020-12-14 15:49:47 +0000

    solve console

--- sketch.js
+++ sketch.js
@@ -108,18 +108,6 @@ function draw() {
 
 }
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.body.style.backgroundColor = "black";
-  } else {
-   document.body.style.backgroundColor = "pink";
-  }
-}
-
-var x = window.matchMedia("(max-width: 550px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
-
 function togglePlaying() {
   if (!song.isPlaying()) {
     song.play();
